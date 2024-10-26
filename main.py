9a6fa3fb68ccf5f0a2e78368e07ba0a404d123c6
ibrahim-shazly22 <ibrahimshazly2001@gmail.com>
2024-10-26 04:54:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import flask
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+def check_winner(board):
+    for row in board:
+        if row.count(row[0]) == len(row) and row[0] != ' ':
+            return True
+
+    for col in range(len(board)):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != ' ':
+            return True
+
+    if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
+        if board[1][1] != ' ':
+            return True
+
+    return False
+
+def is_board_full(board):
+    for row in board:
+        if ' ' in row:
+            return False
+    return True
+
+def tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    players = ['X', 'O']
+    turn = 0
+
+    while True:
+        print_board(board)
+
+        player = players[turn % 2]
+        print(f"Player {player}'s turn.")
+
+        row = int(input("Enter row (0, 1, 2): "))
+        col = int(input("Enter column (0, 1, 2): "))
+
+        if board[row][col] == ' ':
+            board[row][col] = player
+            if check_winner(board):
+                print_board(board)
+                print(f"Player {player} wins!")
+                break
+            elif is_board_full(board):
+                print_board(board)
+                print("It's a draw!")
+                break
+            turn += 1
+        else:
+            print("That position is already taken. Try again.")
+
+if __name__ == "__main__":
+    tic_tac_toe()
